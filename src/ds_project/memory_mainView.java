24cebba5e4b0d58e3d98eda911db575bf064ecbc
martijn vanallemeersch <martijn852@hotmail.be>
2018-10-20 17:38:45 +0200

    half werkende memory/tal van fouten nog + code moet nog gekuist worden!

--- src/ds_project/memory_mainView.java
+++ src/ds_project/memory_mainView.java
@@ -17,6 +17,11 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -32,8 +37,8 @@ public class memory_mainView implements Runnable {
 	private int port = 22222;
 	private Scanner scanner = new Scanner(System.in);
 	private JFrame frame;
-	private final int WIDTH = 506;
-	private final int HEIGHT = 527;
+	private final int WIDTH = 520; //506
+	private final int HEIGHT = 540;
 	private Thread thread;
 
 	private Painter painter;
@@ -49,7 +54,10 @@ public class memory_mainView implements Runnable {
 	private BufferedImage redCircle;
 	private BufferedImage blueCircle;
 
-	private String[] spaces;
+	private Boolean[] spaces;
+	private BufferedImage[] spaces2;
+
+	private BufferedImage[] ImageArray;
 
 	private boolean yourTurn = false;
 	private boolean circle = true;
@@ -61,6 +69,7 @@ public class memory_mainView implements Runnable {
 
 	private int lengthOfSpace;	
 	private int groteGame;
+	private boolean [] checkMemory;
 
 	private int errors = 0;
 	private int firstSpot = -1;
@@ -74,16 +83,41 @@ public class memory_mainView implements Runnable {
 	private String unableToCommunicateWithOpponentString = "Unable to communicate with opponent.";
 
 	
+	Map<Integer, Integer> GelinkteKaartenMemory;
+	int[] checkforGelinkteKaarten;
+	int indexClick = 0;
+	boolean tweeZetten = false;
+	int indexRead = 0;
+	int[] readValues;
+
+	
 	public memory_mainView(int groteGameArg,int lengthOfSpaceArg) {
 		
+			
 		this.lengthOfSpace = lengthOfSpaceArg;
 		this.groteGame = groteGameArg;
-		spaces = new String[groteGameArg * groteGameArg];
+		spaces = new Boolean[groteGameArg * groteGameArg];
 		
-		System.out.println("Please input the IP: ");
-		ip = scanner.nextLine();
-		System.out.println("Please input the port: ");
-		port = scanner.nextInt();
+		for (int i = 0; i < spaces.length; i++) {
+			spaces[i] = false;
+		}
+		
+		spaces2 = new BufferedImage[groteGameArg * groteGameArg];
+		ImageArray = new BufferedImage[groteGameArg * groteGameArg];
+		checkMemory = new boolean [2];
+		GelinkteKaartenMemory = new HashMap<Integer, Integer>();
+		checkforGelinkteKaarten = new int[2];
+		checkforGelinkteKaarten[0]= -1;
+		checkforGelinkteKaarten[1]= -1;
+
+		readValues = new int[2];
+		//fillSpaces();	
+		//System.out.println("Please input the IP: ");
+		//ip = scanner.nextLine();
+		ip = "localhost";
+		//System.out.println("Please input the port: ");
+		//port = scanner.nextInt();
+		port = 22222;
 		while (port < 1 || port > 65535) {
 			System.out.println("The port you entered was invalid, please input another port: ");
 			port = scanner.nextInt();
@@ -117,7 +151,6 @@ public class memory_mainView implements Runnable {
 			if (!circle && !accepted) {
 				listenForServerRequest();
 			}
-
 		}
 	}
 
@@ -134,23 +167,37 @@ public class memory_mainView implements Runnable {
 		}
 
 		if (accepted) {
-			for (int i = 0; i < spaces.length; i++) {
-				if (spaces[i] != null) {
-					if (spaces[i].equals("X")) {
-						if (circle) {
-							g.drawImage(redX, (i % groteGame) * lengthOfSpace + 10 * (i % groteGame), (int) (i / groteGame) * lengthOfSpace + 10 * (int) (i / groteGame), null);
-						} else {
-							g.drawImage(blueX, (i % groteGame) * lengthOfSpace + 10 * (i % groteGame), (int) (i / groteGame) * lengthOfSpace + 10 * (int) (i / groteGame), null);
-						}
-					} else if (spaces[i].equals("O")) {
-						if (circle) {
-							g.drawImage(blueCircle, (i % groteGame) * lengthOfSpace + 10 * (i % groteGame), (int) (i / groteGame) * lengthOfSpace + 10 * (int) (i / groteGame), null);
-						} else {
-							g.drawImage(redCircle, (i % groteGame) * lengthOfSpace + 10 * (i % groteGame), (int) (i / groteGame) * lengthOfSpace + 10 * (int) (i / groteGame), null);
-						}
-					}
+			
+			if(checkforGelinkteKaarten[0] != -1)
+			{				
+				//System.out.println("kaart "+ checkforGelinkteKaarten[0] + " index " + indexClick);
+				
+				if(indexClick == 0)
+				{					
+					g.drawImage(ImageArray[checkforGelinkteKaarten[0]], (checkforGelinkteKaarten[0] % groteGame) * lengthOfSpace + 10 * (checkforGelinkteKaarten[0] % groteGame), (int) (checkforGelinkteKaarten[0] / groteGame) * lengthOfSpace + 10 * (int) (checkforGelinkteKaarten[0] / groteGame), null);				
 				}
-			}			
+				else
+				{	
+					if(checkforGelinkteKaarten[1] != -1)
+					{
+						System.out.println("kaart "+ checkforGelinkteKaarten[1] + " index " + indexClick);
+
+						g.drawImage(ImageArray[checkforGelinkteKaarten[1]], (checkforGelinkteKaarten[1] % groteGame) * lengthOfSpace + 10 * (checkforGelinkteKaarten[1] % groteGame), (int) (checkforGelinkteKaarten[1] / groteGame) * lengthOfSpace + 10 * (int) (checkforGelinkteKaarten[1] / groteGame), null);				
+
+						checkMatch();
+
+					}
+					g.drawImage(ImageArray[checkforGelinkteKaarten[0]], (checkforGelinkteKaarten[0] % groteGame) * lengthOfSpace + 10 * (checkforGelinkteKaarten[0] % groteGame), (int) (checkforGelinkteKaarten[0] / groteGame) * lengthOfSpace + 10 * (int) (checkforGelinkteKaarten[0] / groteGame), null);				
+
+				}
+			}
+			
+			for (int i = 0; i < spaces.length; i++) {
+				if (spaces[i] == true) {
+					g.drawImage(ImageArray[i], (i % groteGame) * lengthOfSpace + 10 * (i % groteGame), (int) (i / groteGame) * lengthOfSpace + 10 * (int) (i / groteGame), null);				
+				}
+			}
+			
 		} else {
 			g.setColor(Color.RED);
 			g.setFont(font);
@@ -160,18 +207,50 @@ public class memory_mainView implements Runnable {
 			g.drawString(waitingString, WIDTH / 2 - stringWidth / 2, HEIGHT / 2);
 		}
 	}
+	
+	private void checkMatch()
+	{	
 
+		if((GelinkteKaartenMemory.get(checkforGelinkteKaarten[0]) == checkforGelinkteKaarten[1]))
+		{
+			//System.out.println("GelinkteKaartenMemory");
+
+			spaces[checkforGelinkteKaarten[0]] = true;
+			spaces[checkforGelinkteKaarten[1]] = true;
+		}		
+	}
 	private void tick() {
 		if (errors >= 10) unableToCommunicateWithOpponent = true;
 
 		if (!yourTurn && !unableToCommunicateWithOpponent) {
 			try {
-				int space = dis.readInt();
-				if (circle) spaces[space] = "X";
-				else spaces[space] = "O";
-				checkForEnemyWin();
-				checkForTie();
-				yourTurn = true;
+				readValues[indexRead] = dis.readInt();
+				System.out.println("read " + readValues[indexRead]);
+							
+
+
+				if(indexRead == 0)
+				{
+					checkforGelinkteKaarten[0] = readValues[0];
+					indexRead = 1 ;
+				}
+				else //indexRead == 1
+				{
+					checkforGelinkteKaarten[0] = readValues[0];
+					checkforGelinkteKaarten[1] = readValues[1];
+					System.out.println("ontvangen"+ checkforGelinkteKaarten[0]);
+					System.out.println("ontvangen" + checkforGelinkteKaarten[1]);
+
+					checkMatch();					
+
+					checkForEnemyWin();
+					checkForTie();
+					indexRead = 0;
+					yourTurn = true;
+				}
+				
+			
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 				errors++;
@@ -229,24 +308,49 @@ public class memory_mainView implements Runnable {
 	}
 
 	private void loadImages() {
-		try {
+		try {		
+			
 			if(groteGame == 3)
 			{
-				board = ImageIO.read(getClass().getResourceAsStream("/board3X3.png"));
-				redX = ImageIO.read(getClass().getResourceAsStream("/redX.png"));
-				redCircle = ImageIO.read(getClass().getResourceAsStream("/redCircle.png"));
-				blueX = ImageIO.read(getClass().getResourceAsStream("/blueX.png"));
-				blueCircle = ImageIO.read(getClass().getResourceAsStream("/blueCircle.png"));
+				
 			}
 			else
 			{
 				board = ImageIO.read(getClass().getResourceAsStream("/board4X4.png"));
-				redX = ImageIO.read(getClass().getResourceAsStream("/redX4X4.png"));
-				redCircle = ImageIO.read(getClass().getResourceAsStream("/redCircle4X4.png"));
-				blueX = ImageIO.read(getClass().getResourceAsStream("/blueX4X4.png"));
-				blueCircle = ImageIO.read(getClass().getResourceAsStream("/blueCircle4X4.png"));
-			}
-			
+				ImageArray[0]= ImageIO.read(getClass().getResourceAsStream("/audi4.jpg"));
+				ImageArray[5]= ImageIO.read(getClass().getResourceAsStream("/audi4.jpg"));
+				ImageArray[1]= ImageIO.read(getClass().getResourceAsStream("/bmw4.jpg"));
+				ImageArray[6]= ImageIO.read(getClass().getResourceAsStream("/bmw4.jpg"));
+				ImageArray[2]= ImageIO.read(getClass().getResourceAsStream("/citroen4.jpg"));
+				ImageArray[7]= ImageIO.read(getClass().getResourceAsStream("/citroen4.jpg"));
+				ImageArray[3]= ImageIO.read(getClass().getResourceAsStream("/mercedes4.jpg"));
+				ImageArray[8]= ImageIO.read(getClass().getResourceAsStream("/mercedes4.jpg"));
+				ImageArray[4]= ImageIO.read(getClass().getResourceAsStream("/mini4.jpg"));
+				ImageArray[9]= ImageIO.read(getClass().getResourceAsStream("/mini4.jpg"));
+				ImageArray[10]= ImageIO.read(getClass().getResourceAsStream("/peugeot4.jpg"));
+				ImageArray[14]= ImageIO.read(getClass().getResourceAsStream("/peugeot4.jpg"));
+				ImageArray[11]= ImageIO.read(getClass().getResourceAsStream("/porsche4.jpg"));
+				ImageArray[15]= ImageIO.read(getClass().getResourceAsStream("/porsche4.jpg"));
+				ImageArray[12]= ImageIO.read(getClass().getResourceAsStream("/volkswagen4.jpg"));
+				ImageArray[13]= ImageIO.read(getClass().getResourceAsStream("/volkswagen4.jpg"));			
+				
+				GelinkteKaartenMemory.put(0,5);
+				GelinkteKaartenMemory.put(5,0);
+				GelinkteKaartenMemory.put(1,6);
+				GelinkteKaartenMemory.put(6,1);
+				GelinkteKaartenMemory.put(2,7);
+				GelinkteKaartenMemory.put(7,2);
+				GelinkteKaartenMemory.put(3,8);
+				GelinkteKaartenMemory.put(8,3);
+				GelinkteKaartenMemory.put(4,9);
+				GelinkteKaartenMemory.put(9,4);
+				GelinkteKaartenMemory.put(10,14);
+				GelinkteKaartenMemory.put(14,10);
+				GelinkteKaartenMemory.put(12,13);
+				GelinkteKaartenMemory.put(13,12);
+				GelinkteKaartenMemory.put(11,15);
+				GelinkteKaartenMemory.put(15,11);
+			}			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -256,6 +360,18 @@ public class memory_mainView implements Runnable {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		memory_mainView ticTacToe = new memory_mainView(4,125);
+	}
+	
+	private void fillSpaces()
+	{	
+		 ArrayList<Integer> list = new ArrayList<Integer>();
+	        for (int i=0; i<16; i++) {
+	            list.add(i);
+	        }
+	        Collections.shuffle(list);
+	        for (int i=0; i<16; i++) {
+	        	spaces2[list.get(i)] = ImageArray[i];
+	        }		
 	}
 
 	private class Painter extends JPanel implements MouseListener {
@@ -277,21 +393,49 @@ public class memory_mainView implements Runnable {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			if (accepted) {
+				
+				//System.out.println("Click");
+				//System.out.println(yourTurn);
+
+
 				if (yourTurn && !unableToCommunicateWithOpponent && !won && !enemyWon) {
 					int x = e.getX() / lengthOfSpace;
 					int y = e.getY() / lengthOfSpace;
 					y *= groteGame;
 					int position = x + y;
+					
+					checkforGelinkteKaarten[indexClick]= position;
+					System.out.println("Click " + checkforGelinkteKaarten[indexClick]);
+					
+					if(indexClick == 0)
+					{
+						tweeZetten = false;
+						indexClick = 1;
+						
+						try {
+							dos.writeInt(position); 
+							dos.flush();
+							System.out.println("flush " + position);
 
-					if (spaces[position] == null) {
-						if (!circle) spaces[position] = "X";
-						else spaces[position] = "O";
-						yourTurn = false;
+						} catch (IOException e1) {
+							errors++;
+							e1.printStackTrace();
+						}
+					}					
+					else 
+					{
+						//System.out.println("gelinkt 0: "+ checkforGelinkteKaarten[0]);
+						//System.out.println("gelinkt 1: "+ checkforGelinkteKaarten[1]);
+
+						tweeZetten = true;
+						
 						repaint();
 						Toolkit.getDefaultToolkit().sync();
+						
+						checkMatch();
 
 						try {
-							dos.writeInt(position);
+							dos.writeInt(position); 
 							dos.flush();
 						} catch (IOException e1) {
 							errors++;
@@ -301,8 +445,12 @@ public class memory_mainView implements Runnable {
 						System.out.println("DATA WAS SENT");
 						checkForWin();
 						checkForTie();
-
-					}
+						
+						indexClick = 0;
+						yourTurn = false;
+						checkforGelinkteKaarten[0] = -1;
+						checkforGelinkteKaarten[1] = -1;
+					}					
 				}
 			}
 		}
